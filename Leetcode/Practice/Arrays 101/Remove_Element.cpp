d428a492f16b2090aa2954e115df64f70134977b
Manav Prajapati <manavprajapati1398@gmail.com>
2021-03-04 10:24:23 +0530

    Organized practice folder

---
+++ Leetcode/Practice/Arrays 101/Remove_Element.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void deleteVal(vector<int>& nums, int index, int len) {
+    for(int i=index+1; i<len; i++) {
+        nums[i-1] = nums[i];
+    }
+    return;
+}
+
+int removeElement(vector<int>& nums, int val) {
+    int len = nums.size();
+    int count = 0;
+    for(int i=0; i<len; i++) {
+        if(nums[i] == val) {
+            deleteVal(nums, i, len);
+            ++count;
+            --len;
+            --i;
+        }
+    }
+    
+    // Printing Details
+    cout << "Output: " << count << endl;
+    cout << "Array: ";
+    for(int i=0; i<len; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    //
+    return len;
+}
+int main() {
+    int n, val;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i=0; i<n; i++) {
+        int temp;
+        cin >> temp;
+        nums[i] = temp;
+    }
+    cin >> val;
+    int m = removeElement(nums, val);
+    // cout << removeElement(nums, val) << endl;
+    // for(auto num : nums) {
+    //     cout << num << " ";
+    // }
+    // cout << endl;
+    return 0;
+}
