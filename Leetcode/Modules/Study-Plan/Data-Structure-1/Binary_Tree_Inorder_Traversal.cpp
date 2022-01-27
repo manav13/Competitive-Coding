81a4944ac1b3b1430bb3cf0c4be02aab904173e1
Manav Prajapati <manavprajapati1398@gmail.com>
2022-01-27 11:37:55 -0600

    Added Day 10 DS module problems of LC

---
+++ Leetcode/Modules/Study-Plan/Data-Structure-1/Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void traversal(TreeNode* node, vector<int> &ans) {
+    //Base Case
+    if(node == NULL) return;
+    
+    // Calculation - LVR
+    traversal(node->left, ans);
+    ans.push_back(node->val);
+    traversal(node->right, ans);
+}
+
+vector<int> inorderTraversal(TreeNode* root) {
+    if(root == NULL) return {};
+    vector<int> ans;
+    traversal(root, ans);
+    return ans;
+}
+
+int main() {
+    return 0;
+}
