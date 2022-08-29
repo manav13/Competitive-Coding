44c14420a6bc00dd6af8d0cd85c3ec99407d1f6e
Manav Prajapati <manavprajapati1398@gmail.com>
2022-08-28 21:55:15 -0500

    updated 1 Grind 75 question

--- Leetcode/Grind 75/21_Majority_Element.cpp
+++ Leetcode/Grind 75/21_Majority_Element.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 class Solution {
 public:
+    // TC = O(n), SC = O(1)
     int majorityElement(vector<int>& nums) {
+        int count = 0, element = nums[0];
+        for(auto num : nums) {
+            if(num == element) count++;
+            else count--;
+            
+            if(count < 0) {
+                element = num;
+                count = 1;
+            }
+        }
+        return element;
+    }
+
+    // TC = O(n), SC = O(n)
+    int majorityElement2(vector<int>& nums) {
         int size = nums.size();
         size = size/2;
         unordered_map<int, int> hash;
