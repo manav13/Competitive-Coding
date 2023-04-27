245dc9331cf2563c0c99e3f63c43e7cb8930636f
Manav Prajapati <manavprajapati1398@gmail.com>
2023-04-27 14:06:02 -0500

    modified MathWorks_Problem_2

--- Company Problems/MathWorks_Problem_2.cpp
+++ Company Problems/MathWorks_Problem_2.cpp
@@ -2,21 +2,27 @@
 using namespace std;
 
 long maxSumLeftToRight(vector<int> nums) {
-    int n = nums.size();
+    
+    vector<long> dupNums;
+    for(auto num : nums) {
+        dupNums.push_back(num);
+    }
+
+    int n = dupNums.size();
     int k = n;
     deque<int> dq{0}; // store index of `nums` elements, elements is in decreasing order, the front is the maximum element.
     for (int i = 1; i < n; ++i) {
         // nums[i] = max(nums[i-k], nums[i-k+1], .., nums[i-1]) + nums[i] = nums[dq.front()] + nums[i]
-        nums[i] = nums[dq.front()] + nums[i];
+        dupNums[i] = dupNums[dq.front()] + dupNums[i];
 
         // Add nums[i] to our deque
-        while (!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back(); // Eliminate elements less or equal to nums[i], which will never be chosen in the future
+        while (!dq.empty() && dupNums[dq.back()] <= dupNums[i]) dq.pop_back(); // Eliminate elements less or equal to nums[i], which will never be chosen in the future
         dq.push_back(i);
 
         // Remove if the last element is out of window size k
         if (i - dq.front() >= k) dq.pop_front();
     }
-    return nums[n - 1];
+    return dupNums[n - 1];
 }
 
 int main() {
